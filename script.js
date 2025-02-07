1f207bb58a1891988477c43567aaab42fc4ee44b
Anshul <avenger720720@gmail.com>
2025-02-07 18:49:47 +0530

    functionality of win

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
-let music = new Audio("music.mp3")
-let turn = new Audio("ting.mp3")
-let gameover = new Audio("gameover.mp3")
-let turnFirst = "X"
+let music = new Audio("music.mp3");
+let turnSound = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let turnFirst = "X";
 
-//Function to change the turn
+// Function to change the turn
 const changeTurn = () => {
-    return turnFirst === "X"?"0": "X"
-}
+  return turnFirst === "X" ? "O" : "X";
+};
 
-//Function to check for a win
+// Function to check for a win
 const checkWin = () => {
+    let boxtexts = document.getElementsByClassName('boxtext')
+    let win = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    win.forEach( e => {
+        if((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !== "")){
 
-}
+        }
+    })
+
+};
 
 // Game Logic
-let boxes = document.getElementsByClassName("box")
-Array.from(boxes).forEach(element => {
-    let boxtext = document.querySelector('.boxtext')
-
-})
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach((element) => {
+  let boxtext = element.querySelector(".boxtext"); 
+  element.addEventListener("click", () => {
+    if (boxtext.innerText === "") {
+      boxtext.innerText = turnFirst; 
+      turnFirst = changeTurn(); 
+      turnSound.play(); 
+      checkWin();
+      document.getElementsByClassName("info")[0].innerText =
+        "Turn for " + turnFirst; 
+    }
+  });
+});
